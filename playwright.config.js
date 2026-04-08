4d0a7aa15abff308cd10716413917fb7f7627c75
Lukas Kundrotas <l.kundrotas@yahoo.com>
2026-04-08 21:58:55 +0300

    Add test stage

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "./tests",
+  timeout: 1000,
   use: {
     baseURL: "http://127.0.0.1:4173"
   },
