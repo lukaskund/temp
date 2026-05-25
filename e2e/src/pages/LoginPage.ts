a456044fcc70fb3341cb5187cb8f17142c7a6a25
Lukas Kundrotas <lukas@keyrock.eu>
2026-05-25 14:39:53 +0300

    test

--- e2e/src/pages/LoginPage.ts
+++ e2e/src/pages/LoginPage.ts
@@ -13,7 +13,7 @@ export class LoginPage extends BasePage {
   }
 
   get submitButton() {
-    return this.page.getByTestId('login-btn');
+    return this.page.getByTestId('login-btn-submit');
   }
 
   get loginPageRoot() {
