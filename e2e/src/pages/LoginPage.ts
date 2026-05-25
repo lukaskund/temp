e503247b153acfc0f4524c2102d24a62deda10d5
Lukas Kundrotas <lukas@keyrock.eu>
2026-05-25 14:53:59 +0300

    Adjust error message

--- e2e/src/pages/LoginPage.ts
+++ e2e/src/pages/LoginPage.ts
@@ -91,7 +91,7 @@ export class LoginPage extends BasePage {
     );
     await this.assertText(
       this.loginErrorAlert,
-      'Invalid email or password.',
+      'Incorrect username or password.',
       'Error message should explain that login credentials are invalid.'
     );
   }
